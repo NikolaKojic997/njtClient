e3dbd6c191f83cba3a1ad56ee369eae63bd977e0
nikolakojic997 <volimdahodam@gmail.com>
2020-09-03 16:32:26 +0200

    Get all projects works!

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
+import axios from 'axios'
+
+
+
+interface Employee  {
+    employeeId: number,
+    employmentDate: Date,
+    identificationNumber: string,
+    name: string,
+    surname: string
+}
 
 interface IProps {
 
 }
 
 interface IState {
-    projects: Array<{
-        name:string,
-        status: string,
-        notes: string
-    }>;
+    users: Employee[],
+    activeItem: number
 
 }
 
@@ -19,17 +27,31 @@ export default class UserTable extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
         this.state ={
-            projects:[{
-                name: "pera",
-                status: "asdasd",
-                notes: "none"
-            },
-             {
-                    name: "pera",
-                    status: "asdasd",
-                    notes: "none"
-             }]
+            users: [],
+            activeItem: 0
         }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    async componentDidMount(){
+
+        await axios.get('http://localhost:8080/employee')
+                    .then(resoult => {
+                        this.setState({
+                            users: resoult.data
+                        })
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    });
+
+    }
+
+    handleClick = (id: number)=> {
+        this.setState({
+            ...this.state,
+            activeItem:id
+        });
     }
 
     render(){
@@ -38,18 +60,25 @@ export default class UserTable extends React.Component<IProps, IState> {
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Status</Table.HeaderCell>
-                        <Table.HeaderCell>Notes</Table.HeaderCell>
+                        <Table.HeaderCell>Surname</Table.HeaderCell>
+                        <Table.HeaderCell>Identification number</Table.HeaderCell>
+                        <Table.HeaderCell>Employment date</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    {this.state.projects.map(element => (
-                        <Table.Row>
+                    {this.state.users.map(element => (
+
+                            <Table.Row key={element.employeeId} active={this.state.activeItem == element.employeeId}
+                                   onClick={() => this.handleClick(element.employeeId)}>
                             <Table.Cell>{element.name}</Table.Cell>
-                            <Table.Cell>{element.status}</Table.Cell>
-                            <Table.Cell>{element.notes}</Table.Cell>
+                            <Table.Cell>{element.surname}</Table.Cell>
+                            <Table.Cell>{element.identificationNumber}</Table.Cell>
+                            <Table.Cell>{element.employmentDate}</Table.Cell>
+
                         </Table.Row>
+
+
                     ))}
 
                 </Table.Body>
